a3070564bb6ddcf264e0fc8eeaee6adf31188d75
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2022-03-19 23:39:05 +0800

    add

---
+++ Source/MyUE427Study03/Characters/CharacterBase.cpp
@@ -0,0 +1,110 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterBase.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+ACharacterBase::ACharacterBase()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	cameraBoom = CreateDefaultSubobject<USpringArmComponent>("CameraBoom");
+	cameraBoom->SetupAttachment(GetRootComponent());
+	cameraBoom->TargetArmLength = 600.0f;
+	cameraBoom->SetRelativeRotation(FRotator(-30, 0, 0));
+
+	followCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	followCamera->SetupAttachment(cameraBoom);
+
+	bUseControllerRotationYaw = false;
+	cameraBoom->bUsePawnControlRotation = true;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	mouseRightHold = false;
+}
+
+// Called when the game starts or when spawned
+void ACharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	playerController = Cast<APlayerController>(GetController());
+	playerController->bShowMouseCursor = true;
+}
+
+void ACharacterBase::MoveForward(float val)
+{
+	if (val == 0 || Controller == nullptr)
+	{
+		return;
+	}
+
+	const FRotator rotation = Controller->GetControlRotation();
+	const FRotator yawRotation(0, rotation.Yaw, 0);
+	const FVector direction = FRotationMatrix(yawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(direction, val);
+}
+
+void ACharacterBase::MoveRight(float val)
+{
+	if (val == 0 || Controller == nullptr)
+	{
+		return;
+	}
+
+	const FRotator rotation = Controller->GetControlRotation();
+	const FRotator yawRotation(0, rotation.Yaw, 0);
+	const FVector direction = FRotationMatrix(yawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(direction, val);
+}
+
+void ACharacterBase::MouseRightPressed()
+{
+	mouseRightHold = true;
+}
+
+void ACharacterBase::MouseRightReleased()
+{
+	mouseRightHold = false;
+}
+
+void ACharacterBase::AddControllerYawInput(float val)
+{
+	if (val == 0 || mouseRightHold == false)
+	{
+		return;
+	}
+	Super::AddControllerYawInput(val);
+}
+
+void ACharacterBase::AddControllerPitchInput(float val)
+{
+	if (val == 0 || mouseRightHold == false)
+	{
+		return;
+	}
+	Super::AddControllerPitchInput(val);
+}
+
+// Called every frame
+void ACharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &ACharacterBase::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ACharacterBase::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &ACharacterBase::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("Lookup", this, &ACharacterBase::AddControllerPitchInput);
+	PlayerInputComponent->BindAction("MouseRight", EInputEvent::IE_Pressed, this, &ACharacterBase::MouseRightPressed);
+	PlayerInputComponent->BindAction("MouseRight", EInputEvent::IE_Released, this, &ACharacterBase::MouseRightReleased);
+	PlayerInputComponent->BindAction("Jump", EInputEvent::IE_Pressed, this, &ACharacterBase::Jump);
+	PlayerInputComponent->BindAction("Jump", EInputEvent::IE_Released, this, &ACharacterBase::StopJumping);
+}
---
+++ Source/MyUE427Study03/Characters/CharacterBase.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "CharacterBase.generated.h"
+
+UCLASS()
+class MYUE427STUDY03_API ACharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACharacterBase();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera")
+	USpringArmComponent* cameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera")
+	UCameraComponent* followCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Player")
+	APlayerController* playerController;
+
+private:
+	bool mouseRightHold;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void MoveForward(float val);
+
+	virtual void MoveRight(float val);
+
+	virtual void MouseRightPressed();
+
+	virtual void MouseRightReleased();
+	
+	virtual void AddControllerYawInput(float val) override;
+
+	virtual void AddControllerPitchInput(float val) override;
+
+	
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
