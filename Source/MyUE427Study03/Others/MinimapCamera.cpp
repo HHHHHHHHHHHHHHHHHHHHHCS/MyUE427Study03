1ebcf96b8d98e67566b84efcc68b4ed9b237f01f
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2022-03-29 00:00:35 +0800

    add

---
+++ Source/MyUE427Study03/Others/MinimapCamera.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MinimapCamera.h"
+
+#include "Components/SceneCaptureComponent2D.h"
+#include "Kismet/GameplayStatics.h"
+
+AMinimapCamera::AMinimapCamera()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	GetCaptureComponent2D()->SetRelativeRotation(FRotator(-90.0f, 0.0f, 0.0f));
+}
+
+void AMinimapCamera::BeginPlay()
+{
+	Super::BeginPlay();
+	mainPlayer = Cast<ACharacterBase>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+void AMinimapCamera::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	OutsideTick();
+}
+
+void AMinimapCamera::OutsideTick()
+{
+	FVector targetLocation = mainPlayer->GetActorLocation();
+	targetLocation.Z = GetActorLocation().Z;
+	SetActorLocation(targetLocation);
+}
---
+++ Source/MyUE427Study03/Others/MinimapCamera.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/SceneCapture2D.h"
+#include "MyUE427Study03/Characters/CharacterBase.h"
+#include "MinimapCamera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API AMinimapCamera : public ASceneCapture2D
+{
+	GENERATED_BODY()
+
+public:
+	ACharacterBase* mainPlayer;
+
+public:
+	AMinimapCamera();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	void OutsideTick();
+};
