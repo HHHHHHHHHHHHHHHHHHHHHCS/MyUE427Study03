cc79c09575db3f07dcc2acd1dfe3369394d9512a
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2022-03-30 00:06:42 +0800

    add

---
+++ Source/MyUE427Study03/UserWidget/UserWidget_Main.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UserWidget_Main.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API UUserWidget_Main : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
