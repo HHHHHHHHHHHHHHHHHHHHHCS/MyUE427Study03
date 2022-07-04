635cd8c498312f5b8d87a4015b7c58fbe9c12445
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2022-07-05 00:34:17 +0800

    add

---
+++ Source/MyUE427Study03/UserWidget/SkillTree/UI_SkillTree_SubTree.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/CanvasPanel.h"
+#include "UI_SkillTree_SubTree.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API UUI_SkillTree_SubTree : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta=(BindWidget))
+	UCanvasPanel* CanvasPanel_Root;
+};
