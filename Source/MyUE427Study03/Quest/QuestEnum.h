ab2bf9848bbaf7ee82f3fb5e7b4fef3869622ae2
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2022-07-26 00:51:02 +0800

    add

---
+++ Source/MyUE427Study03/Quest/QuestEnum.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "QuestEnum.generated.h"
+
+//任务类型, 主线 支线
+UENUM()
+enum class EQuestCategories
+{
+	// 主线任务
+	MainQuest,
+
+	// 支线任务
+	SideQuest,
+
+	// 事件
+	Events,
+};
+
+//任务目标的类型
+UENUM()
+enum class EGlobalTypes
+{
+	//该NPC没有任务
+	Custom,
+
+	//刷怪
+	Hunt,
+
+	//搜索
+	Find,
+
+	//对话
+	Talk,
+};
+
+//任务的区域
+UENUM()
+enum class ERegions
+{
+	BeiJing,
+
+	NanJing,
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API UQuestEnum : public UObject
+{
+	GENERATED_BODY()
+};
