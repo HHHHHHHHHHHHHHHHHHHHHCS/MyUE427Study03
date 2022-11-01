04a9f72477a91308b97300d12fc272de75d1b4b6
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2022-11-02 00:09:59 +0800

    add

---
+++ Source/MyUE427Study03/NPC/NPCBase.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NPCBase.h"
+
+// Sets default values
+ANPCBase::ANPCBase()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	interactionWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("InteractionWidget"));
+	interactionWidget->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ANPCBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ANPCBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ANPCBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/MyUE427Study03/NPC/NPCBase.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "GameFramework/Character.h"
+#include "NPCBase.generated.h"
+
+UCLASS()
+class MYUE427STUDY03_API ANPCBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere)
+	UWidgetComponent* interactionWidget;
+
+	UPROPERTY(EditAnywhere, Category="Quest")
+	FText name;
+
+public:
+	// Sets default values for this character's properties
+	ANPCBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
