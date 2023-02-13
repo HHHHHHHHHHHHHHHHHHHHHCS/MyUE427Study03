0f3c17773a28ab08c1464bc862c732e103d89fcb
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-02-14 00:15:13 +0800

    add

---
+++ Source/MyUE427Study03/InventorySystem/InventoryEnum.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "InventoryEnum.generated.h"
+
+UENUM()
+enum class EItemCategories
+{
+	Consume,
+	Equipment,
+	QuestItem,
+	Readable,
+};
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API UInventoryEnum : public UObject
+{
+	GENERATED_BODY()
+};
---
+++ Source/MyUE427Study03/InventorySystem/InventoryStruct.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InventoryEnum.h"
+#include "UObject/NoExportTypes.h"
+#include "InventoryStruct.generated.h"
+
+USTRUCT()
+struct FItemInfo
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category="ItemInfo")
+	FText name;
+
+	UPROPERTY(EditAnywhere, Category="ItemInfo")
+	FText desc;
+
+	UPROPERTY(EditAnywhere, Category="ItemInfo")
+	UTexture2D* icon;
+
+	UPROPERTY(EditAnywhere, Category="ItemInfo")
+	bool canUsed = true;
+
+	UPROPERTY(EditAnywhere, Category="ItemInfo")
+	FText useText; //使用说明, 比如: 药水-Drink, 地图-Read
+
+	UPROPERTY(EditAnywhere, Category="ItemInfo")
+	bool canStacked; //是否可以堆积
+
+	UPROPERTY(EditAnywhere, Category="ItemInfo")
+	EItemCategories category = EItemCategories::Consume;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API UInventoryStruct : public UObject
+{
+	GENERATED_BODY()
+};
