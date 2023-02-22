220d89f8856c3e3653cd3cd97079563b58a547ea
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-02-23 00:28:22 +0800

    add

---
+++ Source/MyUE427Study03/UserWidget/Inventory/UI_InventorySlot.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI_InventorySlot.h"
+
+#include "Components/Button.h"
+#include "MyUE427Study03/InventorySystem/Inventory.h"
+#include "MyUE427Study03/InventorySystem/ItemBase.h"
+
+void UUI_InventorySlot::UpdateSlot()
+{
+	if (inventoryRef->IsSlotEmpty(slotIndex))
+	{
+		Button_Slot->SetIsEnabled(false);
+		Image_Icon->SetVisibility(ESlateVisibility::Hidden);
+		Text_Amount->SetVisibility(ESlateVisibility::Hidden);
+	}
+	else
+	{
+		Button_Slot->SetIsEnabled(true);
+		AItemBase* item = inventoryRef->GetItemByIndex(slotIndex);
+		itemInfo = item->itemInfo;
+		amount = item->amount;
+		Image_Icon->SetBrushFromTexture(itemInfo.icon);
+		Image_Icon->SetVisibility(ESlateVisibility::HitTestInvisible);
+		if(itemInfo.canStacked)
+		{
+			Text_Amount->SetText(FText::AsNumber(amount));
+			Text_Amount->SetVisibility(ESlateVisibility::HitTestInvisible);
+		}
+		else
+		{
+			Text_Amount->SetVisibility(ESlateVisibility::Hidden);
+		}
+	}
+}
---
+++ Source/MyUE427Study03/UserWidget/Inventory/UI_InventorySlot.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "MyUE427Study03/InventorySystem/InventoryStruct.h"
+#include "UI_InventorySlot.generated.h"
+
+class UButton;
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API UUI_InventorySlot : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Meta=(BindWidget))
+	UButton* Button_Slot;
+
+	UPROPERTY(Meta=(BindWidget))
+	UTextBlock* Text_Amount;
+
+	UPROPERTY(Meta=(BindWidget))
+	UImage* Image_Icon;
+
+protected:
+	int slotIndex;
+	int amount;
+	class AInventory* inventoryRef;
+	FItemInfo itemInfo;
+
+public:
+	void UpdateSlot();
+};
