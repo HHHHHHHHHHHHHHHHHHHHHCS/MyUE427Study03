eea38227c050ecf4b3af97831fd2dfc94305dc55
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-03-09 00:00:40 +0800

    add

---
+++ Source/MyUE427Study03/InventorySystem/Item_ManaPotion.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemBase.h"
+#include "Item_ManaPotion.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API AItem_ManaPotion : public AItemBase
+{
+	GENERATED_BODY()
+	
+};
