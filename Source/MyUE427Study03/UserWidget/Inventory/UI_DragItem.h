e3381e0b2216ff1565f36f78381a39eb0369d851
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-04-07 00:42:46 +0800

    add

---
+++ Source/MyUE427Study03/UserWidget/Inventory/UI_DragItem.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MyUE427Study03/InventorySystem/InventoryStruct.h"
+#include "UI_DragItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API UUI_DragItem : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Image_Icon;
+
+	UPROPERTY(meta=(BindWidget))
+	class UTextBlock* Text_Amount;
+
+	FItemInfo itemInfo;
+	int amount;
+};
