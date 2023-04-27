c46bae90986a58bb74bc91d49c3284c5ec8ed763
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-04-27 21:30:08 +0800

    add

---
+++ Source/MyUE427Study03/UserWidget/Inventory/UI_ItemDetail.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI_ItemDetail.h"
+
+#include "MyUE427Study03/Others/StaticLibrary.h"
+
+void UUI_ItemDetail::UpdateInfo()
+{
+	Text_Name->SetText(itemInfo.name);
+	Text_Amount->SetText(FText::AsNumber(amount));
+	Text_Category->SetText(FText::FromString(UStaticLibrary::GetEnumValueAsString<EItemCategories>("EItemCategories", itemInfo.category)));
+	Text_Description->SetText(itemInfo.desc);
+	Image_Icon->SetBrushFromTexture(itemInfo.icon);
+}
---
+++ Source/MyUE427Study03/UserWidget/Inventory/UI_ItemDetail.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "MyUE427Study03/InventorySystem/InventoryStruct.h"
+#include "UI_ItemDetail.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API UUI_ItemDetail : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta=(BindWidget))
+	UImage* Image_Icon;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* Text_Name;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* Text_Amount;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* Text_Category;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* Text_Description;
+
+public:
+	FItemInfo itemInfo;
+	int amount;
+
+	void UpdateInfo();
+};
