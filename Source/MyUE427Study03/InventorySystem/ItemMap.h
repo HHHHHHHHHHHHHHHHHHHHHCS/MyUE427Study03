0490109b1d8596c1af4b8935422dc4759dfef4f3
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-05-13 13:44:32 +0800

    add

---
+++ Source/MyUE427Study03/InventorySystem/ItemMap.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemBase.h"
+#include "ItemMap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API AItemMap : public AItemBase
+{
+	GENERATED_BODY()
+	
+};
