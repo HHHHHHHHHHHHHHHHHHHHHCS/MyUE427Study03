937021640865435f86cd09e4d5ff804cd1cc86d4
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-07-07 00:42:31 +0800

    add

---
+++ Source/MyUE427Study03/NPC/NPC_Merchant.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NPC_Merchant.h"
+
+void ANPC_Merchant::OnInteractWith(ACharacterBase* character)
+{
+	Super::OnInteractWith(character);
+}
---
+++ Source/MyUE427Study03/NPC/NPC_Merchant.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NPCBase.h"
+#include "NPC_Merchant.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE427STUDY03_API ANPC_Merchant : public ANPCBase
+{
+	GENERATED_BODY()
+
+
+public:
+	virtual void OnInteractWith(ACharacterBase* character) override;
+};
